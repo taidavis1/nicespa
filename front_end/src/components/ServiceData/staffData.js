378973fea678e91353f345c01263c2456dcd629e
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-12-17 17:31:38 -0800

    changes

--- front_end/src/components/ServiceData/staffData.js
+++ front_end/src/components/ServiceData/staffData.js
@@ -17,6 +17,12 @@ const staffData = [
         description: "Susan Yu Lin is a dedicated therapist with over 10 years of professional experience. Known for her genuine warmth and friendly approach, she consistently goes above and beyond to ensure her clients feel comfortable and valued. Susan's expertise, combined with her compassionate nature, has positively influenced countless lives, making her a sought-after therapist in her community",
         img: require('../../img/Staff/staff3.jpg')
     },
+    {
+        id: 4,
+        staffname: 'Amy Zheng',
+        description: "Amy Zheng excels as a Chinese massage therapist, combining professionalism with a friendly demeanor. Her expertise enables her to deliver a 60-minute massage session that feels as comprehensive and rejuvenating as a typical 90-minute session elsewhere. Amy's approach is not only professional but also charming and artistic, making her an exceptional therapist in her field",
+        img: require('../../img/Staff/right.jpg')
+    },
 ];
 
 export default staffData;
