fab92a8b84c3c512e16774346857ae60892873f1
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2024-04-22 23:13:15 -0700

    changes

--- front_end/src/components/Popup.js
+++ front_end/src/components/Popup.js
@@ -2,33 +2,37 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import Popup1 from '../img/test.png';
 
 const Popup = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const images = [Popup1];
+    const handleButtonClick = () => {
+        if (currentImageIndex < images.length - 1) {
+            setCurrentImageIndex(currentImageIndex + 1);
+        } else {
+            setIsOpen(!isOpen);
+        }
+    };
 	const AppointmentBtn = (e) => {
         e.preventDefault();
         window.open("https://nice-spa-106973.square.site/" , '_blank');
     };
     return (
-		<div className={`${!isOpen? "opacity-100" : "hidden" } fixed left-0 top-0 inset-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10`}>
-			<div className="w-full max-w-3xl overflow-y-auto sm:rounded-2xl bg-bgpop bg-center bg-cover bg-no-repeat">
-				<div className='text-end'>
-					<button className = " mr-5 mt-4" onClick={ () => setIsOpen(!isOpen)}>
-						<FontAwesomeIcon className=' text-3xl text-white' icon={faXmark} />
-					</button>
-				</div>
-				<div className="w-full px-4">
-					<div className=" p-7 lg:space-y-6 space-y-4 md:p-24 mx-auto">
-						<div className=" space-y-4 lg:space-y-6">
-							<h1 className="text-center font-Monterast text-gold text-2xl md:text-4xl font-extrabold">New Year Promotion!</h1>
-							<p className="text-[#FFA86D] font-Roboto font-bold text-xl uppercase md:text-4xl text-center">Get 20% off</p>
-						</div>
-						<div className="pb-6 font-Lora flex justify-center text-center">
-							<button onClick={AppointmentBtn} className="p-2 md:p-3 md:text-2xl text-lg bg-black hover:bg-white hover:text-black  hover:-translate-y-2 border-2 border-white rounded-full text-white md:w-full w-3/4 font-semibold">Book now</button>
-						</div>
-					</div>
-				</div>
-			</div>
+		<div onClick={handleButtonClick} className={`${!isOpen? "opacity-100" : "hidden" } px-2 fixed left-0 top-0 inset-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50`}>
+			<div className='relative'>
+                {currentImageIndex === 2 || currentImageIndex === 3? (
+                    <img className='h-[680px] w-[700px] lg:w-[500px] lg:h-[800px]' src={images[currentImageIndex]} />
+                ): (
+                    <img className=' w-[400px] lg:w-[800px] h-[300px] lg:h-[500px]' src={images[currentImageIndex]} />
+                )}
+                <div className=' absolute top-0 right-0'>
+                    <button className = " p-3" onClick={handleButtonClick}>
+                        <FontAwesomeIcon className=' text-2xl text-orange-400' icon={faXmark} />
+                    </button>
+                </div>
+            </div>
 		</div>
     );
 };
