d57f914354f22bae3b1a6c65ff7d8e2788bf0dfe
taimongus <jacobpham82@gmail.com>
2025-08-26 13:24:08 -0700

    Update Staff

--- front_end/src/components/ServiceData/staffData.js
+++ front_end/src/components/ServiceData/staffData.js
@@ -1,34 +1,22 @@
 const staffData = [
     {
         id: 1,
-        staffname: 'Sean Buth',
-        description: "Sean is a CA Certified Professional Massage Therapist. CMTC #91569.  He is a graduated student from The National Holistic Institute of Massage Therapy College in Sacramento with awards of excellence. He has performed 1000 hours of massage therapy and body work on the public and staff at NHI Teaching clinic and Oriental Charm Massage at the time of graduation. His specialty is back and neck pain. He provides you a friendly and amazing therapeutic massage experience.",
-        img: require('../../img/Staff/staffone.jpg')
-    },
-    {
-        id: 2,
         staffname: 'Victoria Nguyen',
         description: "Victoria is a Certified Massage Therapist and Cosmetologist. An alumnus of the National Holistic Institute in Sacramento, she graduated with honors and received excellence accolades. In the realm of cosmetology, Victoria brings a keen eye for beauty and detail, ensuring her clients feel both relaxed and revitalized. She offers a tailored, safe, and exceptional therapeutic massage experience with utmost professionalism.",
         img: require('../../img/Staff/stafftwo.jpg')
     },
     {
-        id: 3,
+        id: 2,
         staffname: 'Susan Yu Lin',
         description: "Susan Yu Lin is a dedicated therapist with over 10 years of professional experience. Known for her genuine warmth and friendly approach, she consistently goes above and beyond to ensure her clients feel comfortable and valued. Susan's expertise, combined with her compassionate nature, has positively influenced countless lives, making her a sought-after therapist in her community",
         img: require('../../img/Staff/staff3.jpg')
     },
     {
-        id: 4,
+        id: 3,
         staffname: 'Amy Zheng',
         description: "Amy Zheng excels as a Chinese massage therapist, combining professionalism with a friendly demeanor. Her expertise enables her to deliver a 60-minute massage session that feels as comprehensive and rejuvenating as a typical 90-minute session elsewhere. Amy's approach is not only professional but also charming and artistic, making her an exceptional therapist in her field",
         img: require('../../img/Staff/right.jpg')
     },
-    {
-        id: 5,
-        staffname: 'Hannah Le',
-        description: 'She is a California state licensed massage therapist with a passion for healing and care. Known for her kindness and compassion, especially toward seniors, she creates a calming space where every session is meaningful. She’s always growing in her craft and finds joy in helping others feel their best. Meeting new people and making a difference through massage is what inspires her every day.',
-        img: require('../../img/Staff/hannah.png')
-    }
 ];
 
 export default staffData;
